6fdb50e804e251a7ffb805581e0a78806aba0376
root-bine <2749775885@qq.com>
2022-07-08 23:08:37 +0800

    新增内容js

---
+++ js/login.js
@@ -0,0 +1,15 @@
+jQuery.get( '/path/to/file',{param1: 'value1'}, function(data,textStatus,xhn){};
+$.ajax({
+    url: '/path/tokfile',
+    type: "default GET (other values: POST)',
+    dataType: 'default: Intelligent Guess (other values: xml, json,script,or html) ',data: fparam1: "value1"},
+})
+.done(function(){
+    consoLe. log("success");
+}
+.failfunction({
+    console.log("error");
+})
+.always(function(){
+    consoLe.log("complete");
+}
