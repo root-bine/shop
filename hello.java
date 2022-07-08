127ed7f799cab57b83c9e4326aa3e9b62bd34584
root-bine <2749775885@qq.com>
2022-07-08 18:24:28 +0800

    修改后的hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class Hello{
   public static void main(String []args){
     System.out.println("Hello world!!!");
+	int age = 18;
+	String name = "张三";
+	System.out.println("姓名:"+name+",年龄:"+age);
   }
 }
